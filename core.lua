88872ddcbc3bec0b41844ab2cbca22050409e437
Maksim Sunjajkin <Maksim.Sunjajkin@bwedu.de>
2018-02-20 11:07:10 +0100

    First functions working

--- core.lua
+++ core.lua
@@ -1,8 +1,30 @@
-local function Dummy_ToolTip()
-	if UnitExists("mouseover") then
-		GameTooltip:AddLine("es klappt xdd");
-		GameTooltip:Show();
-	end;
-end;
+local addonName, db = ...
 
-GameTooltip:HookScript("OnTooltipSetUnit", Dummy_ToolTip);
+local function getScore(characterName, characterRealm)
+	for i, name in ipairs(db.characters) do
+		if name == characterName then
+			return db.score[i]
+		end
+	end
+end
+
+local function getCharacterInfo()
+	unit = GameTooltip:GetUnit()
+	if UnitIsPlayer(unit) then
+		characterName, characterRealm = UnitName(unit)
+		if characterName == nil then
+			characterName, characterRealm = UnitName("mouseover")
+		end
+		GameTooltip:AddLine(characterName)
+
+		if characterRealm == nil or characterRealm == "" then
+			characterRealm = GetRealmName()
+		end
+		GameTooltip:AddLine(characterRealm)
+		score = getScore(characterName, characterRealm)
+		GameTooltip:AddLine(score)
+		GameTooltip:Show()
+	end
+end
+
+GameTooltip:HookScript("OnTooltipSetUnit", getCharacterInfo)
